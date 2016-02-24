3167fdb1381475f8a0b1d302441451e16afefa62
gerasws6 <geras_21@hotmail.com>
2016-02-24 01:22:23 -0600

    Actividad nueva y se genera un switch

--- MyApplication4/app/src/main/java/com/example/geras/myapplication/MainActivity.java
+++ MyApplication4/app/src/main/java/com/example/geras/myapplication/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.geras.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -44,9 +45,16 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            showSettings();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
+
+    private void showSettings() {
+        Intent intent = new Intent(MainActivity.this,Settings.class);
+        startActivity(intent);
+    }
+
 }
